15cfc435636e5593fe7659570799b84922432c07
mrmissong <mrmissong@gmail.com>
2023-02-21 02:14:11 +0530

    panel colors

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -8,38 +8,35 @@ import pic2 from "../../assets/expenseManager.png";
 import pic3 from "../../assets/repayments.png";
 import pic4 from "../../assets/stockCheck.png";
 import Nav from "../../components/nav/Nav";
-
 const Home = () => {
-  return (
-    <div>
-      <Header />
-      <div className="homeBody">
-        <div className="navigation">
-          <Nav />
-        </div>
-        <div className="mainPanel">
-          <Panel
-            panelLogo={pic1}
-            panelName="Add an activiy"
-            panelData="goaTrip"
-            panelClass="cardPanel"
-          />
-          {/* <br /> */}
-          <Panel panelLogo={pic2} panelClass="cardPanel" />
-          {/* <br /> */}
-          <Panel panelLogo={pic3} panelClass="cardPanel" />
-          {/* <br /> */}
-          <Panel panelLogo={pic4} panelClass="cardPanel" />
-          {/* <br /> */}
-        </div>
-      </div>
-      
-      <div className="homeFooter">
-      <hr />
-        <Footer />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<div className="homeBody">
+				<div className="navigation">
+					<Nav />
+				</div>
+				<div className="mainPanel">
+					<Panel
+						panelLogo={pic1}
+						panelName="Add an activiy"
+						panelData="goaTrip"
+					/>
+
+					<Panel panelLogo={pic2} />
+
+					<Panel panelLogo={pic3} />
+
+					<Panel panelLogo={pic4} />
+				</div>
+			</div>
+
+			<div className="homeFooter">
+				<hr />
+				<Footer />
+			</div>
+		</div>
+	);
 };
 
 export default Home;
