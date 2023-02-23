1a4089e9565ed1cea82a07aeca95ff115418a262
ayankhan21 <ayankhanxyz00@gmail.com>
2023-02-23 16:09:08 +0530

    Chart css

--- client/src/containers/ExpenseManager/components/Chart.js
+++ client/src/containers/ExpenseManager/components/Chart.js
@@ -1,25 +1,10 @@
 import React from "react";
 import { Pie, Bar, Doughnut } from "react-chartjs-2";
 import Chart from "chart.js/auto";
-// import axios from "axios";
+import './chart.css'
 const ExpenseChart = ({ pieData }) => {
-	// const data = {
-	// 	labels: ["Income", "Savings", "Total"],
-	// 	datasets: [
-	// 		{
-	// 			label: "My First Dataset",
-	// 			data: [content.income, content.savings, content.total],
-	// 			backgroundColor: [
-	// 				"rgb(255, 99, 132)",
-	// 				"rgb(54, 162, 235)",
-	// 				"rgb(255, 205, 86)",
-	// 			],
-	// 			hoverOffset: 4,
-	// 		},
-	// 	],
-	// };
 	return (
-		<div className="pieChart">
+		<div className="DonutChart">
 			<Doughnut data={pieData} />
 		</div>
 	);
