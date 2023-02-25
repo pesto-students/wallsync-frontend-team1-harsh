e31e6a33393635b4485fdc128c73c283942aabc8
mrmissong <mrmissong@gmail.com>
2023-02-25 14:50:09 +0530

    live market page

--- client/src/containers/LiveMarket/LiveMarket.js
+++ client/src/containers/LiveMarket/LiveMarket.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import Nav from "../../components/nav/Nav";
+import "./livemarket.css";
+// import Button from "../../components/button/Button";
+// import Table from "./components/Table";
+// import Avatar from "../../components/avatar/Avatar";
+import axios from "axios";
+// import ExpenseChart from "./components/Chart";
 
 const LiveMarket = () => {
-  return (
-    <div>LiveMarket</div>
-  )
-}
+	return (
+		<div>
+			<Header />
+			<div className="LiveBody">
+				<Nav />
+				<div className="LiveDashboard">
+					<iframe
+						src={
+							"https://in.widgets.investing.com/top-cryptocurrencies?theme=darkTheme"
+						}
+						width={"100%"}
+						height={"100%"}
+						frameborder={"0"}
+						allowtransparency={"true"}
+						marginwidth={"0"}
+						marginheight={"0"}
+					></iframe>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
+};
 
-export default LiveMarket
+export default LiveMarket;
