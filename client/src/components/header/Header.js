e836e1f961d34f5859501207a9b5b857dfb3da30
ayankhan21 <ayankhanxyz00@gmail.com>
2023-02-26 22:17:54 +0530

    Nav

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -3,18 +3,20 @@ import "./header.css";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 const Header = ({ children, className }) => {
-	return (
-		<div className="header">
-			<Link to="/home">
-				<div className="default">
-					<img src={logo} alt="" />
-					<h1>WALLSYNC</h1>
-				</div>
-			</Link>
-
-			<div className={className}>{children}</div>
-		</div>
-	);
+  return (
+    <>
+      <div className="header">
+        <Link to="/home">
+          <div className="default">
+            <img src={logo} alt="" />
+            <h1>WALLSYNC</h1>
+          </div>
+        </Link>
+        <div className={className}>{children}</div>
+      </div>
+	  <hr className="headerLine"/>
+    </>
+  );
 };
 
 export default Header;
--- client/src/components/nav/Nav.js
+++ client/src/components/nav/Nav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./nav.css";
 import home from "../../assets/icons8-home-48 (1).png";
+import logo from '../../assets/logo.png'
 import menu from "../../assets/icons8-menu-rounded-50.png";
 import wallet from "../../assets/icons8-wallet-64.png";
 // import toggle from "../../assets/icons8-toggle-on-50.png";
@@ -9,53 +10,98 @@ import Toggle from "./toggle/Toggle";
 import { Link } from "react-router-dom";
 const Nav = () => {
   return (
+    // <div className="navbar">
+    //   <div className="sidenav">
+    //     <ul>
+    //       <Link className="navLogo" to="/home">
+    //         <img src={logo} alt="home icon" />
+    //       </Link>
+    //     </ul>
+    //     <ul>
+    //       <Link to="/home">
+    //         <img src={home} alt="home icon" />
+    //         <p>Home</p>
+    //       </Link>
+    //       <Link>
+    //         <img src={menu} />
+    //         <p>Menu</p>
+    //       </Link>
+    //       <Link>
+    //         <img src={wallet} />
+    //         <p>Wallet</p>
+    //       </Link>
+    //       <div className="toggle">
+    //         <Toggle />
+    //         <p>switch</p>
+    //       </div>
+    //       <Link to="/settings">
+    //         <img src={settings} />
+    //         <p>settings</p>
+    //       </Link>
+    //     </ul>
+    //     <ul>
+    //       <Link to="/">
+    //         <img src={home} />
+    //         <p>Logout</p>
+    //       </Link>
+    //     </ul>
+    //   </div>
+    //   <div className="mobilenav">
+    //     <ul className="mobilenavbottom">
+    //       <img src={home} />
+    //       <img src={menu} />
+    //       <img src={wallet} />
+    //       <img src={settings} />
+    //     </ul>
+    //   </div>
+    // </div>
     <div className="navbar">
-      <div className="sidenav">
-        <ul>
-          <Link to="/home">
-            <li>
-              <img src={home} alt="home icon" />
-              <p>Home</p>
-            </li>
-          </Link>
-          <li>
-            <img src={menu} />
-            <p>Menu</p>
-          </li>
-          <li>
-            <img src={wallet} />
-            <p>Wallet</p>
-          </li>
-          {/* <li><img src={toggle}  /><p>switch</p></li> */}
-          <li>
-            <Toggle />
-            <p>switch</p>
-          </li>
-          <Link to="/settings">
-            <li>
-              <img src={settings} />
-              <p>settings</p>
-            </li>
-          </Link>
-        </ul>
-        <ul>
-          <Link to="/">
-            <li>
-              <img src={home} />
-              <p>Logout</p>
-            </li>
-          </Link>
-        </ul>
-      </div>
-      <div className="mobilenav">
-        <ul className="mobilenavbottom">
-          <img src={home} />
-          <img src={menu} />
-          <img src={wallet} />
-          <img src={settings} />
-        </ul>
-      </div>
-    </div>
+			<div className="sidenav">
+				<ul>
+					<Link to="/home">
+						<li>
+							<img src={home} alt="home icon" />
+							<p>Home</p>
+						</li>
+					</Link>
+					<li>
+						<img src={menu} />
+						<p>Menu</p>
+					</li>
+					<li>
+						<img src={wallet} />
+						<p>Wallet</p>
+					</li>
+					{/* <li><img src={toggle}  /><p>switch</p></li> */}
+					<li>
+						<Toggle />
+						<p>switch</p>
+					</li>
+					<Link to="/settings">
+						<li>
+							<img src={settings} />
+							<p>settings</p>
+						</li>
+					</Link>
+				</ul>
+				<ul>
+					<Link to="/">
+						<li>
+							<img src={home} />
+							<p>Logout</p>
+						</li>
+					</Link>
+				</ul>
+			</div>
+			<div className="mobilenav">
+				<ul className="mobilenavbottom">
+					<img src={home} />
+					<img src={menu} />
+					<img src={wallet} />
+					<img src={settings} />
+				</ul>
+			</div>
+		</div>
   );
 };
 
