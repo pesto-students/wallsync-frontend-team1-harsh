aa664b822ebcbf21675ba8f2156be3890082615c
ayankhan21 <ayankhanxyz00@gmail.com>
2023-02-27 14:36:54 +0530

    Budget

--- client/src/App.js
+++ client/src/App.js
@@ -5,92 +5,79 @@ import "./app.css";
 import Login from "./containers/login/Login";
 import Signup from "./containers/signUp/SignUp";
 import Home from "./containers/home/Home";
-import GoogleB from "./components/GB/Google";
 import Profile from "./containers/profile/Profile";
-import Toggle from "./components/nav/toggle/Toggle";
 import Settings from "./containers/settings/Settings";
-import Expense from "./containers/ExpenseManager/Expense";
-import { IconButton } from "@mui/material";
-// import Split from "./containers/split/Split";
 import Repayments from "./containers/Repayments/Repayments";
 import LiveMarket from "./containers/LiveMarket/LiveMarket";
 import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	Link,
-	RouterProvider,
-	Navigate,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link,
+  RouterProvider,
+  Navigate,
 } from "react-router-dom";
 import Budget from "./containers/Budget/Budget";
 import Split from "./containers/split/Split";
 
 const App = () => {
-	// const user = true;
-	const router = createBrowserRouter([
-		{
-			path: "/",
-			element: <Landing />,
-		},
-		{
-			path: "/home",
-			element: <Home />,
-		},
-		{
-			path: "/signup",
-			element: <Signup />,
-		},
-		{
-			path: "/login",
-			element: <Login />,
-		},
-		// {
-		// 	path: "/splitbills",
-		// 	element: <Split />,
-		// },
-		{
-			path: "/settings",
-			element: <Settings />,
-		},
-		{
-			path: "/profile",
-			element: <Profile />,
-		},
-		{
-			path: "/expense",
-			element: <Expense />,
-		},
-		{
-			path: "/repayments",
-			element: <Repayments />,
-		},
-		{
-			path: "/live-market",
-			element: <LiveMarket />,
-		},
-		{
-			path: "/budget",
-			element: <Budget />,
-		},
-		{
-			path: "/split",
-			element: <Split />,
-		},
-	]);
-	return (
-		// <div className="App">
-		// 	{/* <Landing /> */}
-		// 	<Login />
-		// 	{/* <Repayments /> */}
-		// 	{/* <Signup /> */}
-		// 	{/* <Home /> */}
-		// 	{/* <Expense /> */}
-		// 	{/* <LiveMarket /> */}
-		// 	{/* <Profile /> */}
-		// 	{/* <Settings /> */}
-		// 	{/* <SplitBills /> */}
-		// </div>
-		<RouterProvider router={router} />
-	);
+  // const user = true;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landing />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/settings",
+      element: <Settings />,
+    },
+    {
+      path: "/profile",
+      element: <Profile />,
+    },
+    {
+      path: "/repayments",
+      element: <Repayments />,
+    },
+    {
+      path: "/live-market",
+      element: <LiveMarket />,
+    },
+    {
+      path: "/budget",
+      element: <Budget />,
+    },
+    {
+      path: "/split",
+      element: <Split />,
+    },
+  ]);
+  return (
+    // <div className="App">
+    // 	{/* <Landing /> */}
+    // 	<Login />
+    // 	{/* <Repayments /> */}
+    // 	{/* <Signup /> */}
+    // 	{/* <Home /> */}
+    // 	{/* <Expense /> */}
+    // 	{/* <LiveMarket /> */}
+    // 	{/* <Profile /> */}
+    // 	{/* <Settings /> */}
+    // 	{/* <SplitBills /> */}
+    // </div>
+    <RouterProvider router={router} />
+  );
 };
 export default App;
--- client/src/containers/Budget/Budget.js
+++ client/src/containers/Budget/Budget.js
@@ -6,91 +6,112 @@ import Avatar from "../../components/avatar/Avatar";
 import Table from "./components/table/Table";
 import ExpenseChart from "./components/chart/Chart";
 import axios from "axios";
+import LineChart from "./components/chart/LineChart";
 import Footer from "../../components/footer/Footer";
 const Budget = () => {
-	const [content, setContent] = useState([]);
-	const [chartData, setChartData] = useState([]);
-	const [description, setDescription] = useState("");
-	const [amount, setAmount] = useState("");
+  const [content, setContent] = useState([]);
+  const [chartData, setChartData] = useState([]);
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
 
-	useEffect(() => {
-		const getGroup = () => {
-			axios
-				.get("http://localhost:8000/api/63f361935a6870f14f57389d/budget")
-				.then((data) => {
-					// console.log(data.data[0]);
-					setContent(data.data[0].expensesArray);
-					setChartData(data.data[0]);
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		};
-		getGroup();
-	}, [description, amount]);
-	//table data
-	const columns = [
-		{ field: "id", headerName: "ID", width: 70 },
-		{ field: "Description", headerName: "Description", width: 100 },
-		{ field: "Amount", headerName: "Amount", width: 100, sortable: true },
-		{
-			field: "Date",
-			headerName: "Date",
-			type: "number",
-			width: 100,
-			sortable: true,
-		},
-	];
-	const rows = [];
-	{
-		content.map((item, i) => {
-			rows.push({
-				id: i + 1,
-				Description: item.description,
-				Amount: item.amount,
-				Date: item.date.substr(0, 10),
-			});
-		});
-	}
-	const data = {
-		labels: ["Income", "Savings", "Total"],
-		datasets: [
-			{
-				label: "My First Dataset",
-				data: [chartData.income, chartData.savings, chartData.total],
-				backgroundColor: [
-					"rgb(255, 99, 132)",
-					"rgb(54, 162, 235)",
-					"rgb(255, 205, 86)",
-				],
-				hoverOffset: 4,
-			},
-		],
-	};
-	return (
-		<div>
-			<Header children={<Avatar />} />
-			<div className="container">
-				<Nav />
-				<div className="budgetBody">
-					<div className="one">
-						<Table rowData={rows} columnData={columns} />
-						<ExpenseChart pieData={data} />
-					</div>
-					<div className="two">
-						<input></input>
-						<input></input>
-						<button>Add expenses</button>
-					</div>
-					<div className="three">
-						<p>February spendings: {chartData.total}</p>
-						<ExpenseChart pieData={data} />
-					</div>
-				</div>
-			</div>
-			<Footer />
-		</div>
-	);
+  useEffect(() => {
+    const getGroup = () => {
+      axios
+        .get("http://localhost:8000/api/63f361935a6870f14f57389d/budget")
+        .then((data) => {
+          // console.log(data.data[0]);
+          setContent(data.data[0].expensesArray);
+          setChartData(data.data[0]);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getGroup();
+  }, [description, amount]);
+  console.log(content);
+  //table data
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "Description", headerName: "Description", width: 100 },
+    { field: "Amount", headerName: "Amount", width: 100, sortable: true },
+    {
+      field: "Date",
+      headerName: "Date",
+      type: "number",
+      width: 100,
+      sortable: true,
+    },
+  ];
+  const rows = [];
+  {
+    content.map((item, i) => {
+      rows.push({
+        id: i + 1,
+        Description: item.description,
+        Amount: item.amount,
+        Date: item.date.substr(0, 10),
+      });
+    });
+  }
+
+  const lineChartData = {
+    labels: content.map((item) => item.date.substr(5, 5)),
+    // labels:["jan","feb","Mar","apr","may"],
+    datasets: [
+      {
+        label: content.map((item,i) => item.description),
+        data: content.map((item) => item.amount),
+        fill: false,
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.1,
+      },
+    ],
+  };
+
+  const data = {
+    labels: ["Income", "Savings", "Total"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [chartData.income, chartData.savings, chartData.total],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
+  return (
+    <div>
+      <Header children={<Avatar />} />
+      <div className="container">
+        <Nav />
+        <div className="budgetBody">
+          <div className="one">
+            <Table rowData={rows} columnData={columns} />
+            <ExpenseChart pieData={data} />
+          </div>
+          <div className="two">
+            <form action="">
+              <input type="number" placeholder="Amount"></input>
+              <input type="text" placeholder="Description"></input>
+              <button>Add expenses</button>
+			  <input type="text" placeholder="Set limit"/>
+			  <input type="text" placeholder="Add Income"/>
+            </form>
+          </div>
+          <div className="three">
+            <p>February spendings: {chartData.total}</p>
+            <LineChart rowData={lineChartData} />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Budget;
