e98f443b328e1f3b9f819132f0eff5adc4453e4e
mrmissong <mrmissong@gmail.com>
2023-03-30 18:33:16 +0530

    backend deployment changed

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,6 @@
 const config = {
-	apiUrl: "https://wallsync-backend.vercel.app/api",
+	// apiUrl: "https://wallsync-backend.vercel.app/api",
+	apiUrl: "https://wallsyncbackend.onrender.com/api",
 	getUserId: () => {
 		const user = JSON.parse(localStorage.getItem("user"));
 		if (user && user.access_token) {
